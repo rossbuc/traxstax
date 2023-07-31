30bbabd3977d6df34d06395b7d07b887641a0459
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-31 10:05:44 +0100

    Add create new tracklist route

--- controllers/tracklist_controller.py
+++ controllers/tracklist_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Blueprint, redirect
+from flask import Flask, render_template, Blueprint, redirect, request
 from models.user import User
 from models.song import Song
 from models.tracklist import Tracklist
@@ -7,3 +7,20 @@ from app import db
 
 tracklist_blueprint = Blueprint("tracklists", __name__)
 
+@tracklist_blueprint.route("/users/<id>/tracklists/new")
+def new_tracklist(id):
+    songs = Song.query.all()
+    return render_template("tracklists/tracklist_new.jinja", songs=songs, id=id)
+
+@tracklist_blueprint.route("/users/<id>/tracklists", methods=['POST'])
+def create_tracklist(id):
+    name = request.form['title']
+    cover_image = request.form['description']
+    user_id = id
+    # songs = request.form['songs']
+    public = "on" in request.form['private_public']
+    tracklist_to_create =  Tracklist(name=name, cover_image=cover_image, user_id=user_id, public=public)
+    print(tracklist_to_create)
+    db.session.add(tracklist_to_create)
+    db.session.commit()
+    return redirect("/users/1/tracklists")
