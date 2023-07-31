b9ecd9c28aaa887fb56d6e24c36e8f745ddd2095
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-31 12:04:12 +0100

    Add edit tracklist route

--- controllers/tracklist_controller.py
+++ controllers/tracklist_controller.py
@@ -15,9 +15,8 @@ def new_tracklist(id):
 @tracklist_blueprint.route("/users/<id>/tracklists", methods=['POST'])
 def create_tracklist(id):
     name = request.form['title']
-    cover_image = request.form['description']
+    cover_image = request.form['cover_image']
     user_id = id
-    # songs = request.form['songs']
     public = "on" in request.form['private_public']
     tracklist_to_create =  Tracklist(name=name, cover_image=cover_image, user_id=user_id, public=public)
     print(tracklist_to_create)
@@ -29,3 +28,25 @@ def create_tracklist(id):
 def show_tracklist(id, tracklist_id):
     tracklist = Tracklist.query.get(tracklist_id)
     return render_template("tracklists/tracklist_show.jinja", tracklist=tracklist, id=id)
+
+@tracklist_blueprint.route("/users/<id>/tracklists/edit/<tracklist_id>")
+def edit_tracklist(id, tracklist_id):
+    tracklist = Tracklist.query.get(tracklist_id)
+    return render_template("tracklists/tracklist_edit.jinja", tracklist=tracklist, id=id)
+
+@tracklist_blueprint.route("/users/<id>/tracklists/<tracklist_id>", methods=['POST'])
+def update_tracklist(id, tracklist_id):
+    name = request.form['title']
+    cover_image = request.form['cover_image']
+    user_id = id
+    public = "on" in request.form['private_public']
+    
+    tracklist = Tracklist.query.get(tracklist_id)
+
+    tracklist.name = name
+    tracklist.cover_image = cover_image
+    tracklist.user_id = user_id
+    tracklist.public = public
+
+    db.session.commit()
+    return redirect(f"/users/{id}/tracklists/{tracklist_id}")
