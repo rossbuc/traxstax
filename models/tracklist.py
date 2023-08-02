37e588b9df0ee53482ae03c71d2ea9cf6dcdc321
Ross Buchan <rossbuchan1@googlemail.com>
2023-08-02 22:05:41 +0100

    Add styling to edit tracklist page

--- models/tracklist.py
+++ models/tracklist.py
@@ -4,7 +4,7 @@ class Tracklist(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
-    cover_image = db.Column(db.String(64))
+    cover_image = db.Column(db.String(128), default="Enter cover image URL")
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     public = db.Column(db.Boolean)
     songs = db.relationship('Tracklists', backref='tracklist')
--- models/user.py
+++ models/user.py
@@ -6,6 +6,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64))
     password = db.Column(db.String(64))
-    profile_image = db.Column(db.String(64))
+    profile_image = db.Column(db.String(128))
     bio = db.Column(db.Text())
     tracklists = db.relationship("Tracklist", backref="tracklist")
